e877098667be7d8e76d0022ebc4fb70d0ee372ee
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 02:00:25 -0400

    Create first.py

---
+++ first.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+#Print string
+print("hello, world") #double quotes
+print("\n") #new line
+print('Hello, world!') #single quotes
+
+print("""This string runs
+multiple lines!""") #tripe quotes for multiple lines
+
+print("This string is " + "awesome!") #concat
