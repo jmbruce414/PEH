b9ab088c6bed8ccc6d61d697c639d463982905c1
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 03:48:44 -0400

    Update new-script.py

--- new-script.py
+++ new-script.py
@@ -31,3 +31,20 @@ print(letter.lower() in word.lower())
 
 movie = "The Hangover"
 print("My favorite movie is {}.".format(movie))
+
+print('\n')
+#Dictionaries - key/value pairs {}
+drinks = {"White Russian": 7, "Old Fashion": 10, "Lemon Drop": 8}
+print(drinks)
+
+employees = {"Finance": ["Bob", "Linda", "Tina"], "IT": ["Gene", "Louise", "Teedy"], "HR": ["Jimmy Jr.", "Mort"]}
+print(employees)
+employees['Legal'] = ["Mr.  Frond"] # add new key:pair value
+print(employees)
+employees.update({"Sales": ["Andie", "Ollie"]})
+print(employees)
+
+drinks['White Russian'] = 8
+print(drinks)
+
+print(drinks.get("White Russian"))
