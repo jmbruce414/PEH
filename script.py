6627a7d1c5d9628efbeac6d4f679a5635af0b0e1
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 02:19:06 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+#Variables and Methods
+quote = "All is fair in love and war."
+print(quote.upper()) #uppercase
+print(quote.lower()) #lowercase
+print(quote.title()) #titlecase
+
+print(len(quote))
+
+name ="Heath" #string
+age = 30 #int int(30)
+gpa = 3.7 #float float(3.7)print
+
+print(int(age))
+print(int(30.1))
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+age += 1
+print(age)
+
+birthday = 1
+
+age+=1
+print(age)
