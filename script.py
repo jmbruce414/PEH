25979cb1f493ecac73f1b84818c9b8aca5dc2159
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 02:30:18 -0400

    Update script.py

--- script.py
+++ script.py
@@ -23,4 +23,36 @@ print(age)
 birthday = 1
 
 age+=1
-print(age)
+print(str(age) + "\n")
+
+#functions
+print("Here is an example function: ")
+
+def who_am_i(): #this is a function
+	name = "Heath"
+	age = 30
+	print("My name is " + name + " and I am " + str(age) + " years old.")
+
+who_am_i()
+
+#adding parameters
+def add_one_hundred(num):
+	print(num + 100)
+
+add_one_hundred(100)
+
+#multiple params
+def add(x,y):
+	print(x+y)
+
+add(7,8)
+
+def multiply(x,y):
+	return x*y
+
+print(multiply(7,7))
+
+def square_root(x):
+	print(x ** .5)
+
+square_root(64)
