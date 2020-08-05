1e5f2a760d743762fff55d3f30b99923469b15ee
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 02:08:11 -0400

    Create math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+#Math
+print(50 + 50) #add
+print(50-50) #sub
+print(50*50) #mult
+print(50/50) #div
+
+print(50+50-50*50/50) #PEMDAS
+
+print(50**50) #exponent
+
+print(50 % 6) #modulo
+
+print(50/6)
+print(50//6) #integer division, no remainder
