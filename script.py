99ba394be252099df7d57ab7e296fee0689d2e61
jmbruce414 <jmbruce414@gmail.com>
2020-08-05 03:13:29 -0400

    Update script.py

--- script.py
+++ script.py
@@ -56,3 +56,101 @@ def square_root(x):
 	print(x ** .5)
 
 square_root(64)
+
+def nl():
+	print('\n')
+
+nl()
+
+#boolean expressions
+print("Boolean expressions:")
+
+bool1=True
+bool2=3*3 == 9
+bool3=False
+bool4= 3*3 != 9
+
+print(bool1,bool2,bool3,bool4)
+print(type(bool1))
+
+nl()
+#relational and boolean operatiors
+greater_than = 7>5
+less_than= 5<7
+greater_than_equal_to= 7>=5
+less_than_equal_to= 7<=7
+
+test_and= (7>5) and (5<7) #True
+test_and2= (7>5) and (5>7) #false
+test_or= (7>5) or (5<7) #true
+test_or2= (7>5) or (5>7) #true
+
+test_not = not True #False
+
+nl()
+#conditional statements
+def drink(money):
+	if money >=2:
+		return "You've got yourself a drink"
+	else:
+		return "No drink for you"
+
+print(drink(3))
+print(drink(1))
+
+def alcohol(age,money):
+	if (age>=21) and (money>=5):
+		return "We're getting a drink"
+	elif (age>=21) and (money<5):
+		return "Come back with more money"
+	elif (age<21) and (money>=5):
+		return "Nice, try kid"
+	else:
+		return "Youre too poor and too young"
+
+print(alcohol(21,5))
+print(alcohol(21,4))
+print(alcohol(20,4))
+print(alcohol(20,5))
+
+nl()
+#lists - have brackets []
+movies = ["When Harry Met Sally", "The Hangover", "The Perks of Being a Wallflower", "The Exorcist"]
+
+print(movies[0])
+print(movies[1])#second element
+print(movies[1:4])#elements 1-3, exclusive 4
+print(movies[0:])#first element through last
+print(movies[:2])#up to element 2
+print(movies[-1]) #last item
+
+print(len(movies))
+movies.append("JAWS")
+print(movies)
+
+movies.pop()
+print(movies)
+
+movies.pop(0)
+print(movies)
+
+nl()
+#tuples - do not change, ()
+grades = ("a", "b", "c", "d", "f")
+print(grades[1])
+
+nl()
+#looping
+
+#for loops - start to finish of an iterate
+vegetables = ["cucumber", "spinach", "cabbage"]
+for x in vegetables:
+	print(x)
+
+#while loops - execute as long as conditon is true
+
+i=1
+
+while i<10:
+	print( i)
+	i+=1
